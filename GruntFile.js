72d8fcebb1b7b73311d7c7cd928b76dcd6e3458c
bibiuc <bbuc@qq.com>
2016-04-24 17:55:31 +0800

    不知道符不符合大神的要求

--- GruntFile.js
+++ GruntFile.js
@@ -1,25 +1,47 @@
 'user strict'
 
 module.exports=function(grunt){
+    var path=require("path");
     require("load-grunt-tasks")(grunt);
     //Bingo.js List
     var bg_core=["console","core","datavalue","Event","variable","Class","linq","equals","fetch","package","route","cache"],
-        bg_mv=["linkToDom","module","factory","model","observer","ajax","compiles","view","filter","render bingo.mv"],
-        bg_mv_factory=["base","linq","location","render","timeout bingo.mv.factory"],
+        bg_mv=["linkToDom","module","factory","model","observer","ajax","compiles","view","filter","render"],
+        bg_mv_factory=["base","linq","location","render","timeout"],
         bg_mv_cmp=["base"],
         bg_mv_command=["action","attrs","event","for","route","html","if","include","model","styles","text","include","node","base"],
         bg_mv_filter=["base"];
-    // Project configuration.
+    var join=function(dir,arr){arr.forEach(function(name,i){arr[i]=path.join(dir,name+".js")});return arr;};
+    join("src",bg_core);
+    join("src/mv",bg_mv);
+    join("src/mv/factory",bg_mv_factory);
+    join("src/mv/cmp",bg_mv_cmp);
+    join("src/mv/command",bg_mv_command);
+    join("src/mv/filter",bg_mv_filter);
+    var mixined=bg_core.concat(bg_mv,bg_mv_factory,bg_mv_cmp,bg_mv_command,bg_mv_filter);
+    //console.log(mixined);
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-        uglify: {
-            options: {
-                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+        clean:["dist/**"],
+        uglify:{
+            options:{
+                beautify:false,
+                exportAll:true,
+                "DEBUG": true
             },
-            build: {
-                src: 'src/<%= pkg.name %>.js',
-                dest: 'build/<%= pkg.name %>.min.js'
+            min:{
+                files:{
+                    "dist/bingo.min.js":["dist/bingo.js"]
+                }
+            }
+        },
+        concat: {
+            options:{
+                separator: ';',
+            },
+            dist:{
+                dest:"dist/bingo.js",src:mixined
             }
         }
     });
+    grunt.registerTask("default",["clean","concat","uglify"]);
 };
